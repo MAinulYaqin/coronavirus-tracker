6c1db07375f75e8801348107fbb6a2cb77612c6e
MAinulYaqin <ainul.y9b@gmail.com>
2020-06-11 21:42:13 +0700

    UI update & add button to show github

--- app/src/main/java/com/gabutproject/coronavirus_tracking/about/AboutActivity.kt
+++ app/src/main/java/com/gabutproject/coronavirus_tracking/about/AboutActivity.kt
@@ -1,15 +1,26 @@
 package com.gabutproject.coronavirus_tracking.about
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.gabutproject.coronavirus_tracking.R
+import com.gabutproject.coronavirus_tracking.databinding.AboutActivityBinding
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding =
+            DataBindingUtil.setContentView<AboutActivityBinding>(this, R.layout.about_activity)
+
+        binding.lifecycleOwner = this
+
+        binding.startGithubItem.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/ai-null"))
+            startActivity(browserIntent)
+        }
 
         setTitle(R.string.about)
-
-        setContentView(R.layout.about_activity)
     }
 }
