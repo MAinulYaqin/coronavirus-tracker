5aee234dae2bc40d957b295b08762de83c608658
MAinulYaqin <ainul.y9b@gmail.com>
2020-06-11 23:55:13 +0700

    all done except library used button

--- app/src/main/java/com/gabutproject/coronavirus_tracking/about/AboutActivity.kt
+++ app/src/main/java/com/gabutproject/coronavirus_tracking/about/AboutActivity.kt
@@ -7,20 +7,40 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.gabutproject.coronavirus_tracking.R
 import com.gabutproject.coronavirus_tracking.databinding.AboutActivityBinding
+import kotlinx.android.synthetic.main.about_app_card.view.*
 
 class AboutActivity : AppCompatActivity() {
+    private lateinit var binding: AboutActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding =
-            DataBindingUtil.setContentView<AboutActivityBinding>(this, R.layout.about_activity)
+        binding = DataBindingUtil.setContentView(this, R.layout.about_activity)
 
         binding.lifecycleOwner = this
 
-        binding.startGithubItem.setOnClickListener {
-            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/ai-null"))
-            startActivity(browserIntent)
+        // set appBar's title
+        setTitle(R.string.about)
+
+        // item listener
+        onItemClick()
+    }
+
+    private fun startBrowserActivity(url: String) {
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(browserIntent)
+    }
+
+    private fun onItemClick() {
+        binding.aboutCard.star_github_item.setOnClickListener {
+            startBrowserActivity("https://github.com/ai-null/coronavirus-tracker.git")
         }
 
-        setTitle(R.string.about)
+        binding.aboutCard.license_item.setOnClickListener {
+            startBrowserActivity("https://raw.githubusercontent.com/ai-null/coronavirus-tracker/master/LICENSE")
+        }
+
+        binding.aboutCard.api_item.setOnClickListener {
+            startBrowserActivity("https://covid19api.com")
+        }
     }
 }
